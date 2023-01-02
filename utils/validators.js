8cf345f4866b92e37ba65c8f976fe2e8d2993c2a
Honey Tyagi <honeyt290@gmail.com>
2023-01-02 15:20:59 +0530

    first commit

---
+++ utils/validators.js
@@ -0,0 +1,12 @@
+const validateName = (name) => {
+    const nameRegex = new RegExp(/[a-zA-Z] [a-zA-Z ]+[a-zA-Z]$/);
+    return nameRegex. test (name);
+};
+    const valigateEnail = (email) = {
+        const emailRegex = new RegExp(
+            /^[A-Za-z0-9_!#$% *\/=7" {|}. -14€[A-Za-20-9.-1+$/,
+    gn
+    1]
+    return emailRegex.test(email); //x checks whether the entered emai
+    bg
+    
