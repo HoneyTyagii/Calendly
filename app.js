194a4d4a5385953bf59803c98e8d583c331fecf1
Honey Tyagi <honeyt290@gmail.com>
2023-01-06 01:13:30 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+// const app = express();
 const connectDB = require('./config/db');
 require("dotenv").config();
 
@@ -8,13 +8,23 @@ const userRoutes = require('./routes/userRoutes');
 const scheduleRoutes = require('./routes/scheduleRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 
-connectDB();
-app.set(cors());
+const PORT = process.env.PORT;
+// connectDB();
+// app.set(cors());
 
+const app = express();
+connectDB(); //Database Connection
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//Routes
+//app.use("/api", require(".routes/api"))
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/schedule', scheduleRoutes);
 app.use('/api/v1/event', eventRoutes);
 
-app.listen(process.env.PORT,() => {
-    console.log('server is running')
-})
+app.listen(PORT,() => {
+    console.log('server is running on port ${PORT}');
+});
